0c687bfe5e8f27e1c0c868ee6077d763bb94751b
alfonsojk <alfonso.juarezkelly@gmail.com>
2019-07-18 20:29:22 -0500

    pinche server bien chingon

--- PersonModel.js
+++ PersonModel.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+
+const mongoose = require ('mongoose')
+
 const URL_MONGO = 'mongodb+srv://alfonsojk:sacksjk!1@alfonsojk-mdswh.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose.connect(URL_MONGO,{ useNewUrlParser: true },(err)=>{
     if (!err) console.log('Conexión exitosa');
 });
 
+
 const Schema = mongoose.Schema;
 
-const listaSchema = new Schema({
+const PersonSchema = new Schema({
     name :String,
     alias:String,
     first_appearance:String,
@@ -19,10 +22,9 @@ const listaSchema = new Schema({
     alive:Boolean,
     gender:String,
     inspired_by_true_story:Boolean
-
 })
 
 
-const lista = mongoose.model('Lista',listaSchema);
+const Person = mongoose.model('person',PersonSchema);
 
-module.exports = {Lista}
+module.exports = {Person}
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require ('mongoose')
+const {Person} = require ('./PersonModel')
 const PORT = 3000
 
 const app = express();
+
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json());
@@ -11,60 +12,56 @@ app.use(bodyParser.json());
 
 
 app.get('/',(req,res)=>{
-    res.send({message:'Server on C:'})
+    res.send({message:'Server on'})
 });
 
-//Create Esquema
-app.post('/create/esquema',(req,res) => {
-   const {name,alias,first_appearance, psycho_pathology,creation_year,survivor_victims,favorite_weapon, actor, alive, gender, inspired_by_true_story } = req.body; 
-   const newLista = Lista({
-       name:"String",
-       alias:"String",
-       first_appearance:"String",
-       psycho_pathology:"String",
-       creation_year:"String",
-       survivor_victims:"String",
-       favorite_weapon:"String",
-       actor:"String",
-       alive:Boolean,
-       gender:"String",
-       inspired_by_true_story:Boolean,
-       
-   });
-   newesquema.save((err,pelicula)=>{
-       err 
-       ? res.status(409).send(err) 
-       : res.status(201).send(esquema)
-   });
-});
 
-app.get('/all/peliculas',(req,res) => {
-    Pelicula.find().exec()
-        .then(peliculas => res.send(peliculas))
+// Create
+app.post('/create/person',(req,res) => {
+    const {name,alias,first_appearance, psycho_pathology, creation_year, number_of_victims, notable_victims,favorite_weapon, actor, alive, gender, inspired_by_true_story } = req.body; 
+    const newPerson= Person({
+        name,
+        alias,
+        first_appearance, 
+        psycho_pathology, 
+        creation_year, 
+        number_of_victims, 
+        notable_victims, 
+        favorite_weapon,
+        actor,
+        alive,
+        gender,
+        inspired_by_true_story,
+    });
+    newPerson.save((err,person)=>{
+        err 
+        ? res.status(409).send(err) 
+        : res.status(201).send(person)
+    });
+ });
+    
+// Read
+app.get('/all/person',(req,res) => {
+    Person.find().exec()
+        .then(data => res.send(data))
         .catch(err => res.status(409).send(err))
 });
 
-app.get('/pelicula/:id',(req,res)=> {
+app.get('/person/:id',(req,res)=> {
     const {id} = req.params;
-    Pelicula.findById(id).exec()
-        .then(pelicula => pelicula ? res.send(pelicula) : res.status(404).send({message:'Pelicula not found'}))
+    Person.findById(id).exec()
+        .then(data => data ? res.send(data) : res.status(404).send({message:'Person not found'}))
         .catch(err => res.status(409).send(err))
 });
 
-app.put('/pelicula/:id',(req,res) => {
+//Update
+app.put('/person/:id',(req,res) => {
     const {id} = req.params;
-    Pelicula.findByIdAndUpdate(id,{$set:req.body},{new:true}).exec()
-        .then(pelicula => res.send(pelicula))
+    Person.findByIdAndUpdate(id,{$set:req.body},{new:true}).exec()
+        .then(data => res.send(data))
         .catch(err => res.status(409).send(err))
 });
 
-
-app.post('/add/director/:idPelicula',(req,res) => {
-    const {idPelicula} = req.params;
-    Pelicula.findByIdAndUpdate(idPelicula,{$push: {directores:[req.body]}},{new:true}).exec()
-        .then(pelicula => res.status(201).send(pelicula))
-        .catch(err => res.status(409).send(err))
-});
 
 
 app.listen(PORT,() => {
